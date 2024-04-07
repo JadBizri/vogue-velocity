9c7b633dda2b73983cadd391bc21c035bd9d4790
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2024-04-07 14:02:58 -0400

    Added an additional validation error message to email

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const userSchema = new Schema({
     firstName: {type: String, required: [true, 'First name is required']},
     lastName: {type: String, required: [true, 'Last name is required']},
-    email: {type: String, required: [true, 'Email is required'], unique: true},
+    email: {type: String, required: [true, 'Email is required'], unique: [true, 'Email already exists']},
     password: {type: String, required: [true, 'Password is required']}
 });
 
