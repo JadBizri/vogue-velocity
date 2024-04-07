3cef1be9a1f008790c7e493af0ef6c8a4514e559
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2024-04-07 13:09:30 -0400

    Added flash, session modules

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const itemRoutes = require('./routes/itemRoutes');
+const userRoutes = require('./routes/userRoutes');
 const mongoose = require('mongoose');
-const model = require('./models/item');
+const item = require('./models/item');
+const user = require('./models/user');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 
 //create app
 const app = express();
@@ -28,11 +33,26 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
+app.use(session({
+    secret: '123456',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://jadb:jadb123@project3.1gd4hzw.mongodb.net/nbda-project3' })
+}));
+app.use(flash());
+
+app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.successMessages = req.flash('success');
+    res.locals.errorMessages = req.flash('error');
+    next();
+});
 
 //set up route
 app.get('/', (req, res, next) => {
     // Call the findRandom method on your Mongoose model
-    model.findRandom()
+    item.findRandom()
         .then(item => {
             // Render the item/show view
             res.render('index', { item });
@@ -41,6 +61,7 @@ app.get('/', (req, res, next) => {
 });
 
 app.use('/items', itemRoutes);
+app.use('/', userRoutes);
 
 app.use((req, res, next) => {
     let err = new Error('The server cannot locate ' + req.url);
