bad89a0b0c7a79b746a21638b225e369880b52f8
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2024-04-25 16:13:02 -0400

    Added offer routes

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const controller = require('../controllers/itemController');
+const itemController = require('../controllers/itemController');
+const offerController = require('../controllers/offerController');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const { validateId } = require('../middlewares/validator');
-const { isLoggedIn, isSeller } = require('../middlewares/auth');
+const { isLoggedIn, isSeller, isNotSeller, isActive } = require('../middlewares/auth');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.join(__dirname, '..', 'public', 'images'));
@@ -15,20 +16,19 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-router.get('/', controller.index);
+//item routes
+router.get('/', itemController.index);
+router.get('/search', itemController.search);
+router.get('/new', isLoggedIn, itemController.new);
+router.post('/', isLoggedIn, upload.single('image'), itemController.create);
+router.get('/:id', validateId, itemController.show);
+router.get('/:id/edit', validateId, isLoggedIn, isSeller, itemController.edit);
+router.put('/:id', validateId, isLoggedIn, isSeller, itemController.update);
+router.delete('/:id', validateId, isSeller, isLoggedIn, itemController.delete);
 
-router.get('/search', controller.search);
-
-router.get('/new', isLoggedIn, controller.new);
-
-router.post('/', isLoggedIn, upload.single('image'), controller.create);
-
-router.get('/:id', validateId, controller.show);
-
-router.get('/:id/edit', validateId, isLoggedIn, isSeller, controller.edit);
-
-router.put('/:id', validateId, isLoggedIn, isSeller, controller.update);
-
-router.delete('/:id', validateId, isSeller, isLoggedIn, controller.delete);
+//offer routes
+router.post('/:id/offer', validateId, isLoggedIn, isNotSeller, isActive, offerController.create);
+router.get('/:id/offers', validateId, isLoggedIn, isSeller, offerController.show);
+router.post('/:id/offer/accept', validateId, isLoggedIn, isSeller, offerController.accept);
 
 module.exports = router;
